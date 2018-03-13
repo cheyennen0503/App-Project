6f95872cf0409b01514a35f3c63205b27c21de4e
brittanym3578 <brittanym3578@hstat.org>
2018-03-13 16:01:47 +0000

    merge conflict

--- models/model.rb
+++ models/model.rb
@@ -10,15 +10,10 @@ result = JSON.parse(response)
 
 class Job_word
 attr_reader :job_word, :find_word
-<<<<<<< HEAD
-    def initialize (job_word, find_word)
-        @job_word = job_word
-        @find_word = ' '
-=======
+
     def initialize (find_word)
         @find_word = find_word
 
->>>>>>> 44263cf407b484358886cfb2815386c24ca10f10
     end
         def job(find_word)
         begin
